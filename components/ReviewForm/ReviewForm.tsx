996b41a7a6283434a23d5c80b286510815c0a12e
EugKa <eugenekovalenko0@gmail.com>
2021-07-22 10:46:58 +0300

    created review and ReviewForm components

---
+++ components/ReviewForm/ReviewForm.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ReviewFormProps } from './ReviewForm.props';
+import cn from 'classnames';
+import styles from './ReviewForm.module.css';
+import { Input } from '../Input/Input';
+import { Rating } from '../Rating/Rating';
+import { Textarea } from '../Textarea/Textarea';
+import { Button } from '../Button/Button';
+import CloseIcon from './icons/close.svg';
+
+export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
+    return (
+        <>
+            <div 
+                className={cn(styles.reviewForm, className)}
+                {...props}
+            >
+                <Input placeholder="Имя"/>
+                <Input className={styles.title} placeholder="Заголовок отзыва"/>
+                <div className={styles.rating}>
+                    <span>Оценка:</span>
+                    <Rating rating={0}/>
+                </div>
+                <Textarea placeholder="Текст отзыва" className={styles.description}/>
+                <div className={styles.submit}>
+                    <Button appearance="primary">
+                        Отправить
+                    </Button>
+                    <span className={styles.info}>* Перед публикацией озыв пройдет предварительную модерацию и проверку</span>
+                </div>
+            </div>
+            <div className={styles.success}>
+                <div className={styles.successTitle}>
+                    Ваш отзыв отправлен
+                </div>
+                <div>
+                    Спасибо, ваш отзыв будет опубликован после проверки.
+                </div>
+                <CloseIcon className={styles.close}/>
+            </div>
+        </>
+    );
+};
